faccc0f07f44281aef4ffdf01061d15cfb641fe6
Endi You <saykent@gmail.com>
2020-07-23 03:18:15 +1000

    enable flex.js first

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// import FlexDemo from './flex';
-// import FlexDemo from './flexDirection';
-// import FlexDemo from './justifyContent';
-import FlexDemo from './alignItems';
+import FlexDemo from './flex';
+// // import FlexDemo from './flexDirection';
+// // import FlexDemo from './justifyContent';
+// import FlexDemo from './alignItems';
 
 export default function App() {
   return FlexDemo();
--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 리액트네이티브에서 UI 래이아웃을 작성할 때 사용하는 플랙스박스(Flexbox)에대해서 간단히 코드를 작성해 보면서 설명한 블로그에서 사용된 예제 코드입니다.
 
 블로그 링크 : https://tech.10000lab.xyz/reactnative/react-native-flex.html
+
+# 실행 환경
+
+## expo 환경에서 작성되었습니다.
+
+expo를 설치해야하면 아래 링크를 참고하세요.
+
+> https://docs.expo.io/get-started/installation/
